d3d1838e3a30a6d5bcaf0eb56088ba805a597d49
Lebedev Nikita <y87cgp@gmail.com>
2026-09-17 15:44:07 +0400

    Release v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.openwhispr.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
