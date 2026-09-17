dafb6275060c2cc37577a784da628610ab25ec68
Lebedev Nikita <y87cgp@gmail.com>
2026-09-17 17:27:24 +0400

    Release v0.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.openwispr.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.4.4"
+        versionCode = 11
+        versionName = "0.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
